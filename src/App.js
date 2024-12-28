fb2b1909c07ae1b83b8790cfcf2e1731d9e6a844
MJ <capou.mj@gmail.com>
2024-12-28 12:39:43 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-title">Total Page Views</div>
+          <div className="stat-value">89,400</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+      </div>
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default App;
